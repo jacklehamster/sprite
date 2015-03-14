75983d8add5e4c607bca51f74d6ce0d017de0b19
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-13 23:59:55 -0700

    Sprite animation system

---
+++ asset.js
@@ -0,0 +1,148 @@
+(function() {
+    
+    function getMeatlyAsset() {
+        var src = "meatlyjam01spritesheet.png";
+        var spriteData = {
+            "dude1_walk_left": [
+                {src:src,"source-crop":[512*0,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*1,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*2,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*3,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*4,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*5,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*6,0,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*7,0,512,512],hotspot:[256,498]}
+            ],
+            "dude1_walk_right": [
+                {src:src,"source-crop":[512*7,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*6,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*5,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*4,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*3,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*2,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*1,512,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*0,512,512,512],hotspot:[256,498]}
+            ],
+            "dude1_stand_left": [
+                {src:src,"source-crop":[512*0,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_stand_right": [
+                {src:src,"source-crop":[512*1,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_smile_left": [
+                {src:src,"source-crop":[512*2,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_smile_right": [
+                {src:src,"source-crop":[512*3,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_frown_left": [
+                {src:src,"source-crop":[512*4,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_frown_right": [
+                {src:src,"source-crop":[512*5,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_look_left": [
+                {src:src,"source-crop":[512*6,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude1_look_right": [
+                {src:src,"source-crop":[512*7,512*2,512,512],hotspot:[256,498]}
+            ],
+            "dude2_walk_left": [
+                {src:src,"source-crop":[512*0,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*1,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*2,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*3,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*4,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*5,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*6,512*3,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*7,512*3,512,512],hotspot:[256,498]}
+            ],
+            "dude2_walk_right": [
+                {src:src,"source-crop":[512*7,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*6,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*5,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*4,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*3,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*2,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*1,512*4,512,512],hotspot:[256,498]},
+                {src:src,"source-crop":[512*0,512*4,512,512],hotspot:[256,498]}
+            ],
+            "dude2_stand_left": [
+                {src:src,"source-crop":[512*0,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_stand_right": [
+                {src:src,"source-crop":[512*1,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_smile_left": [
+                {src:src,"source-crop":[512*2,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_smile_right": [
+                {src:src,"source-crop":[512*3,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_frown_left": [
+                {src:src,"source-crop":[512*4,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_frown_right": [
+                {src:src,"source-crop":[512*5,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_look_left": [
+                {src:src,"source-crop":[512*6,512*5,512,512],hotspot:[256,498]}
+            ],
+            "dude2_look_right": [
+                {src:src,"source-crop":[512*7,512*5,512,512],hotspot:[256,498]}
+            ],
+            "desk_left": [
+                {src:src,"source-crop":[512*0,512*6,512,512],hotspot:[256,470]}
+            ],
+            "desk_right": [
+                {src:src,"source-crop":[512*1,512*6,512,512],hotspot:[256,470]}
+            ],
+            "balldude_left": [
+                {src:src,"source-crop":[512*2,512*6,512,512],hotspot:[256,498]}
+            ],
+            "balldude_right": [
+                {src:src,"source-crop":[512*3,512*6,512,512],hotspot:[256,498]}
+            ],
+            "cloud_left": [
+                {src:src,"source-crop":[512*4,512*6,512,512],hotspot:[256,498]}
+            ],
+            "cloud_right": [
+                {src:src,"source-crop":[512*5,512*6,512,512],hotspot:[256,498]}
+            ],
+            "demon_right": [
+                {src:src,"source-crop":[512*6,512*6,512,512],hotspot:[256,498]}
+            ],
+            "demon_left": [
+                {src:src,"source-crop":[512*7,512*6,512,512],hotspot:[256,498]}
+            ],
+            "tablet": [
+                {src:src,"source-crop":[512*0,512*7,512,512],hotspot:[256,484]}
+            ],
+            "burger": [
+                {src:src,"source-crop":[512*1,512*7,512,512],hotspot:[256,410]}
+            ],
+            "NES": [
+                {src:src,"source-crop":[512*2,512*7,512,512],hotspot:[256,410]}
+            ],
+            "hat": [
+                {src:src,"source-crop":[512*3,512*7,512,512],hotspot:[256,463]}
+            ],
+            "torch": [
+                {src:src,"source-crop":[512*4,512*7,512,512],hotspot:[256,493]}
+            ],
+            "title": [
+                {src:src,"source-crop":[512*5,512*7,512,512],hotspot:[256,400]}
+            ],
+            "logo": [
+                {src:src,"source-crop":[512*6,512*7,512,512],hotspot:[256,490]}
+            ],
+            "dildo": [
+                {src:src,"source-crop":[512*7,512*7,512,512],hotspot:[256,318]}
+            ]
+        };
+        return spriteData;
+    }
+    
+    window.getMeatlyAsset = getMeatlyAsset;
+    
+})();
---
+++ sprite.js
@@ -0,0 +1,198 @@
+(function() {
+
+    var recyclingBin = [];  //  object pool
+    
+    function createSprite(asset,options) {
+        if(!options) options = {};
+
+        var div;
+        if(recyclingBin.length) {
+            div = recyclingBin.pop();
+        }
+        else {
+            div = document.createElement("div");
+            div.style.position = "absolute";
+            div.style.posLeft = div.style.left = "0px";
+            div.style.posTop = div.style.top = "0px";
+            div.style.pointerEvents = "none";
+        }
+        
+        
+        var labels = [];
+        div.asset = asset;
+        div.index = 0;
+        div.labels = labels;
+        div.scale = 1;
+        div.backgroundSizes = {};
+        div.pos = {x:0,y:0};
+        div.fps = 30;
+        div.hotspotVisible = false;
+        
+        var imgsToLoad = {};
+        var count = 0;
+
+        for(var label in asset) {
+            if(!div.label) div.label = label;
+            var sequence = asset[label];
+            labels.push(label);
+            for(var i=0;i<sequence.length;i++) {
+                var data = sequence[i];
+                if(!imgsToLoad[data.src]) {
+                    imgsToLoad[data.src] = true;
+                    count++;
+                    checkBackgroundSize(div,data,
+                        function(src,img) {
+                            count--;
+                            if(!count) div.ready = true;
+                        }
+                    );
+                }
+            }
+        }
+        
+        //  PUBLIC FUNCTIONS
+        div.update = update;
+        div.next = next;
+        div.animate = animate;
+        div.gotoAndPlay = gotoAndPlay;
+        div.setScale = setScale;
+        div.moveTo = moveTo;
+        div.setHotspotVisible = setHotSpotVisible;
+
+        div.update();
+        div.animate(typeof(options.animate)=="undefined" || option.animate);
+        
+        return div;
+    }
+    
+    //  This function checks the size of a background image used. When scaling, we need to adjust that size
+    function checkBackgroundSize(div,data,callback) {
+        var img = new Image();
+        img.addEventListener("load",
+            function(event) {
+                div.backgroundSizes[data.src] = [img.naturalWidth,img.naturalHeight];
+                callback();
+            });
+        img.src = data.src;
+    }
+    
+    //  Change the label
+    function gotoAndPlay(label) {
+        var div = this;
+        if(div.labels.indexOf(label)>=0 && div.label!=label) {
+            div.index = 0;
+            div.label = label;
+            div.update();
+        }
+    }
+    
+    //  Remove and recycle the sprite
+    function destroy() {
+        var div = this;
+        if(div.parentElement) {
+            div.parentElement.removeChild(div);
+        }
+        if(recyclingBin.indexOf(div)<0) {
+            recyclingBin.push(div);
+        }
+    }
+    
+    //  This is the refresh function, readjusting the parameters when something has changed
+    function update() {
+        var div = this;
+        if(div.ready) {
+            var sequence = div.asset[div.label];
+            var data = sequence[div.index%sequence.length];
+            div.style.backgroundImage = "url("+data.src+")";
+            var sourceCrop = data["source-crop"];
+            div.style.backgroundPositionX = -sourceCrop[0]*div.scale+"px";
+            div.style.backgroundPositionY = -sourceCrop[1]*div.scale+"px";
+            div.style.width = sourceCrop[2]*div.scale+"px";
+            div.style.height = sourceCrop[3]*div.scale+"px";
+            var hotSpot = data.hotspot;
+            div.style.marginLeft = -hotSpot[0]*div.scale+"px";
+            div.style.marginTop = -hotSpot[1]*div.scale+"px";
+            var backgroundSize = div.backgroundSizes[data.src];
+            div.style.backgroundSize = div.scale*backgroundSize[0]+"px " +           
+                div.scale*backgroundSize[1]+"px ";
+            updateHotSpotPosition(div);
+        }
+    }
+    
+    //  Change the scale
+    function setScale(value) {
+        var div = this;
+        div.scale = value;
+        div.update();
+    }
+    
+    //  Move the sprite
+    function moveTo(x,y) {
+        var div = this;
+        div.pos.x = x;
+        div.pos.y = y;
+        div.style.transform = "translate("+x+"px,"+y+"px)";
+    }
+    
+    function next() {
+        var div = this;
+        var sequence = div.asset[div.label];
+        div.index = (div.index+1)%sequence.length;
+    }
+    
+    function animate(value) {
+        var div = this;
+        if(value!=div.animated) {
+            div.animated = value;
+
+
+            var lastAnimation = 0;
+            function performAnimation(now) {
+                var timeDiff = now-lastAnimation;
+                if(timeDiff>1000/div.fps) {
+                    div.next();
+                    div.update();
+                    lastAnimation = now;
+                }
+                
+                if(div.animated)
+                    requestAnimationFrame(performAnimation);
+            }
+            requestAnimationFrame(performAnimation);
+        }
+    }
+    
+    function updateHotSpotPosition(div) {
+        if(hotspotDiv) {
+            var sequence = div.asset[div.label];
+            var data = sequence[div.index%sequence.length];
+            var hotSpot = data.hotspot;
+            hotspotDiv.style.marginLeft = (hotSpot[0]*div.scale-10)+"px";
+            hotspotDiv.style.marginTop = (hotSpot[1]*div.scale-10)+"px";
+        }
+    }
+    
+    var hotspotDiv = null;
+    function setHotSpotVisible(value) {
+        var div = this;
+        if(value) {
+            div.hotspotVisible = value;
+            if(!hotspotDiv) {
+                var cross = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAP0lEQVQ4je2TsQ0AMAzCrD6e0+nQA2JV7RYkNmQWAKlA2awFxuTW09YBDvASGKhAOsMZd2P/pnnKAD8CtexoN6jSK/Ocry2hAAAAAElFTkSuQmCC";
+                var img = new Image();
+                img.src = cross;
+                div.appendChild(img);
+                hotspotDiv = img;
+                updateHotSpotPosition(div);
+            }
+        }
+        else {
+            if(hotspotDiv) {
+                div.removeChild(hotspotDiv);
+                hotspotDiv = null;
+            }
+        }
+    }
+    
+    window.createSprite = createSprite;
+})();
